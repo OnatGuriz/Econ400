102c383bcffa6aaac205f99c3af6af1662af5380
Onat <canguriz@gmail.com>
2022-01-03 15:26:03 +0300

    Output is given as dictionaries now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Created on Thu Nov 11 13:07:32 2021
 @author: cangu
 """
 from Functions import *
+import matplotlib.pyplot as plt
 import config
 
 inputOrNo = input("Do you want to input the values Y/N?")
@@ -65,7 +66,7 @@ if (inputOrNo == "N"):
 
     config.ar_1 = 2
     config.ar_2 = 1
-    config.as_1 = 3
+    config.as_1 = 9
     config.as_2 = 3
 
 #seperates the Nash Eqm into players moves in order to graph
@@ -85,15 +86,27 @@ def sortresult(input):
 
 config.SavingTalents = [1, 2, 3 ,4 ,5 ,6, 7, 8 , 9 ,10]
 config.RecogTalents = [1, 2, 3, 4, 5, 6, 7,8 , 9 ,10]
+
+print(NECompute())
 print(sortresult(getValueNE()))
-def Grapher_Recog():
+
+def Grapher(variable_name):
     resultpl_1_s = []
     resultpl_1_r = []
     resultpl_2_s = []
     resultpl_2_r = []
     resultNE = []
-    for value in config.RecogTalents:
-        config.ar_1 = value
+    if(variable_name == "Saving"):
+        var = config.SavingTalents
+    if(variable_name == "Recognition"):
+        var = config.RecogTalents
+    for value in var:
+        if(variable_name == "Saving"):
+            config.as_1 = value
+        elif(variable_name == "Recognition"):
+            config.ar_1 = value
+        else:
+            exit(2)
         resultNE = sortresult(getValueNE())
         if (resultNE != ([],[],[],[])):
             resultpl_1_s.append(resultNE[0])
@@ -105,32 +118,21 @@ def Grapher_Recog():
             resultpl_1_r.append("NaN")
             resultpl_2_s.append("NaN")
             resultpl_2_r.append("NaN")
-    return resultpl_1_s, resultpl_1_r, resultpl_2_s, resultpl_2_r
+    resdict_1_s = {}
+    resdict_1_r = {}
+    resdict_2_s = {}
+    resdict_2_r = {}
 
-def Grapher_Saving(possibleTalentsS):
-    resultpl_1_s = []
-    resultpl_1_r = []
-    resultpl_2_s = []
-    resultpl_2_r = []
-    resultNE = []
-    for value in config.SavingTalents:
-        config.as_1 = value
-        resultNE = sortresult(getValueNE())
-        if (resultNE != ([],[],[],[])):
-            resultpl_1_s.append(resultNE[0])
-            resultpl_1_r.append(resultNE[1])
-            resultpl_2_s.append(resultNE[2])
-            resultpl_2_r.append(resultNE[3])
-        if(resultNE == ([],[],[],[])):
-            resultpl_1_s.append("NaN")
-            resultpl_1_r.append("NaN")
-            resultpl_2_s.append("NaN")
-            resultpl_2_r.append("NaN")
-    return resultpl_1_s, resultpl_1_r, resultpl_2_s, resultpl_2_r
+    for i in range(len(config.SavingTalents)):
+        resdict_1_s[config.SavingTalents[i]] = resultpl_1_s[i]
+        resdict_1_r[config.SavingTalents[i]] = resultpl_1_r[i]
+        resdict_2_s[config.SavingTalents[i]] = resultpl_2_s[i]
+        resdict_2_r[config.SavingTalents[i]] = resultpl_2_r[i]
 
-print(Grapher_Recog()[0])
-print(Grapher_Recog()[1])
-print(Grapher_Recog()[2])
-print(Grapher_Recog()[3])
+    return resdict_1_s,resdict_1_r , resdict_2_s, resdict_2_r
 
-#ToDo index these new lists as dictionaries with respect to the ar_1 and as_1 values then graph them
+
+
+print(Grapher("Saving")[0])
+
+#ToDo graph the dictionaries with respect to the keys
